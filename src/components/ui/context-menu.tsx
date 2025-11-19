dd5448b3e79c7064bf2c5d78885684f8d0691221
zaazhafrnn <150093563+zaazhafrnn@users.noreply.github.com>
2025-11-19 19:39:22 +0700

    add spacing margin when opening context menu

--- src/components/ui/context-menu.tsx
+++ src/components/ui/context-menu.tsx
@@ -95,6 +95,7 @@ function ContextMenuSubContent({
 
 function ContextMenuContent({
   className,
+  style,
   ...props
 }: React.ComponentProps<typeof ContextMenuPrimitive.Content>) {
   return (
@@ -105,6 +106,7 @@ function ContextMenuContent({
           "bg-popover text-popover-foreground data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2 z-99 max-h-(--radix-context-menu-content-available-height) min-w-[8rem] origin-(--radix-context-menu-content-transform-origin) overflow-x-hidden overflow-y-auto rounded-md border p-1 shadow-md",
           className,
         )}
+        style={{ marginLeft: "0.2rem", ...style }}
         {...props}
       />
     </ContextMenuPrimitive.Portal>
